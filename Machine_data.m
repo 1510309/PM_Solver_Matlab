87ec5ac00e14ace1a471706c92c641994f14c020
Ravi Sundaria <39758425+RaviSundaria@users.noreply.github.com>
2018-09-09 16:43:43 +0300

    Update Machine_data.m

--- Machine_data.m
+++ Machine_data.m
@@ -3,17 +3,17 @@
 % Machine data file 
 data.N_pl=2; % number of parallel paths
 data.poles=4; % number of poles
-data.st_out=0.310;% stator outer diameter
-data.st_in=.200; % stator inner diameter
-data.ro_out=0.1964;% rotor outer diameter
-data.ro_in=0.05; % rotor inner diameter
-data.Xb=0.0972; % satator end winding reactance of a phase at 50 Hz
-data.Rs=0.0654; % Resistance of stator phase winding
-data.Lc=0.246; % effective length of stator core
-data.Hc= -7.4590e+05; %magnet coercivity
-data.Br=0.987; % magnet remanence
-data.Sn =  1.644e-04;% the area of conductor reigon in a stator slot
+data.st_out=0.310;% stator outer diameter in meters
+data.st_in=.200; % stator inner diameter in meters
+data.ro_out=0.1964;% rotor outer diameter in meters
+data.ro_in=0.05; % rotor inner diameter in meters
+data.Xb=0.0972; % satator end winding reactance of a phase at 50 Hz in ohms
+data.Rs=0.0654; % Resistance of stator phase winding in ohms
+data.Lc=0.246; % effective length of stator core in meters
+data.Hc= -7.4590e+05; %magnet coercivity A/m
+data.Br=0.987; % magnet remanence in tesla
+data.Sn =  1.644e-04;% the area of conductor reigon in a stator slot in meter^2
 data.Ncn=12; % number of conductors in a stator slot
 data.filling= 0.96; % filling factor of iron 
-data.cond_pm=7.064e+05;% conductivity of magnets
+data.cond_pm=7.064e+05;% conductivity of magnets 1/ (ohm*meter)
 
